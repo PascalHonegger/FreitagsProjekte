54759ba660430567ee51c0fa00828154e27eef1c
Pascal Honegger <pascal.honegger.external@atos.net>
2015-11-06 14:16:54 +0100

    Added EventHandlers

--- SpaceInvaders/SpaceInvaders/SpaceInvaderViewModel.cs
+++ SpaceInvaders/SpaceInvaders/SpaceInvaderViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using SpaceInvaders.Ships;
+using SpaceInvaders.Ships.EventArgs;
 using SpaceInvaders.Ships.Invader;
 using SpaceInvaders.Ships.Player;
 
@@ -9,32 +10,52 @@ namespace SpaceInvaders
 {
 	internal class SpaceInvaderViewModel
 	{
-		public static readonly Size PlayAreaSize = new Size(400, 300);
 		public const int MaximumPlayerShots = 3;
 		public const int InitialStarCount = 50;
-		private readonly Random _random = new Random();
-		public int Score { get; private set; }
-		public int Wave { get; private set; }
-		public bool GameOver { get; private set; }
-		private DateTime? _playerDied = null;
-		public bool PlayerDying => _playerDied.HasValue;
-		private IPlayer _player;
+		public static readonly Size PlayAreaSize = new Size(400, 300);
 		private readonly List<IInvader> _invaders = new List<IInvader>();
-		private readonly List<IShot> _playerShot = new List<IShot>();
 		private readonly List<IShot> _invaderShots = new List<IShot>();
+		private readonly List<IShot> _playerShot = new List<IShot>();
+		private readonly Random _random = new Random();
 		private readonly List<Point> _stars = new List<Point>();
 		private Direction _invaderDirectio = Direction.Left;
 		private bool _justMovedDown = false;
 		private DateTime _lastUpdated = DateTime.MinValue;
+		private IPlayer _player;
+		private DateTime? _playerDied = null;
 
 		public SpaceInvaderViewModel()
 		{
 			EndGame();
 		}
 
+		public int Score { get; private set; }
+		public int Wave { get; private set; }
+		public bool GameOver { get; private set; }
+		public bool PlayerDying => _playerDied.HasValue;
+
+		public event EventHandler<ShipChangedEventArgs> ShipChangedEventHandler;
+		public event EventHandler<ShotMovedEventArgs> ShotMovedEventHandler;
+		public event EventHandler<StarChangedEventArgs> StarChangedEventHandler;
+
 		public void EndGame()
 		{
 			GameOver = true;
+		}
+
+		protected void OnShipChangedEventHandler(ShipChangedEventArgs e)
+		{
+			ShipChangedEventHandler?.Invoke(this, e);
+		}
+
+		protected void OnShotMovedEventHandler(ShotMovedEventArgs e)
+		{
+			ShotMovedEventHandler?.Invoke(this, e);
+		}
+
+		protected void OnStarChangedEventHandler(StarChangedEventArgs e)
+		{
+			StarChangedEventHandler?.Invoke(this, e);
 		}
 	}
 }
