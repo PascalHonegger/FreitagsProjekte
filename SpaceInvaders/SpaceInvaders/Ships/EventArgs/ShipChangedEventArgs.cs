4e176164bb9a09d6be625d18b0a401b1d0679504
Alain Keller <alain.keller@edu.tbz.ch>
2015-11-06 14:11:25 +0100

    ShipChangedEventARGS

--- SpaceInvaders/SpaceInvaders/Ships/EventArgs/ShipChangedEventArgs.cs
+++ SpaceInvaders/SpaceInvaders/Ships/EventArgs/ShipChangedEventArgs.cs
@@ -8,5 +8,13 @@ namespace SpaceInvaders.Ships.EventArgs
 {
 	class ShipChangedEventArgs
 	{
+		public IShip ShipUpdated { get; private set; }
+		public bool Killed { get; private set; }
+
+		public ShipChangedEventArgs(IShip shipUpdated, bool killed)
+		{
+			ShipUpdated = shipUpdated;
+			Killed = killed;
+		}
 	}
 }
