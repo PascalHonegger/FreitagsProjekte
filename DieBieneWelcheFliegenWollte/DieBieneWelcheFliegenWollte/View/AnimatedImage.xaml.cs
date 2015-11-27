66f2dee2f0defb27f6c0a588203adb0fa4943b7b
Pascal Honegger <pascal.honegger.external@atos.net>
2015-11-27 10:07:16 +0100

    Copy-Paste book v1

---
+++ DieBieneWelcheFliegenWollte/DieBieneWelcheFliegenWollte/View/AnimatedImage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace DieBieneWelcheFliegenWollte.View
+{
+	/// <summary>
+	/// Interaction logic for AnimatedImage.xaml
+	/// </summary>
+	public partial class AnimatedImage : UserControl
+	{
+		public AnimatedImage()
+		{
+			InitializeComponent();
+		}
+
+		public AnimatedImage(IEnumerable<string> imageNames, TimeSpan interval) : this()
+		{
+			StartAnimation(imageNames, interval);
+		}
+
+		public void StartAnimation(IEnumerable<string> imageNames, TimeSpan interval)
+		{
+			var storyboard = new Storyboard();
+			var animation = new ObjectAnimationUsingKeyFrames();
+			Storyboard.SetTarget(animation, Image);
+			Storyboard.SetTargetProperty(animation, new PropertyPath("Source"));
+
+			var currentInterval = TimeSpan.FromMilliseconds(0);
+			foreach (var imageName in imageNames)
+			{
+				var keyFrame = new DiscreteObjectKeyFrame
+				{
+					Value = CreateImageFromAssets(imageName),
+					KeyTime = currentInterval
+				};
+				animation.KeyFrames.Add(keyFrame);
+				currentInterval = currentInterval.Add(interval);
+			}
+			storyboard.RepeatBehavior = RepeatBehavior.Forever;
+			storyboard.AutoReverse = true;
+			storyboard.Children.Add(animation);
+			storyboard.Begin();
+		}
+
+		private static BitmapImage CreateImageFromAssets(string imageName)
+		{
+			return new BitmapImage(new Uri("ms-appx:///Assets/" + imageName));
+		}
+	}
+}
---
+++ DieBieneWelcheFliegenWollte/DieBieneWelcheFliegenWollte/View/FlyingBees.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace DieBieneWelcheFliegenWollte.View
+{
+	/// <summary>
+	/// Interaction logic for FlyingBees.xaml
+	/// </summary>
+	public partial class FlyingBees : Page
+	{
+		public FlyingBees()
+		{
+			InitializeComponent();
+			// navigationHelper = new NavigationHelper(this);
+			// navigationHelper.LoadState += navigatioHelper_LoadState;
+			// navigationHelper.SaveState += navigatioHelper_SaveState;
+
+			var imageNames = new List<string>
+			{
+				"biene1.png",
+				"biene2.png",
+				"biene3.png"
+			};
+
+			FirstBee.StartAnimation(imageNames, TimeSpan.FromMilliseconds(50));
+			SecondBee.StartAnimation(imageNames, TimeSpan.FromMilliseconds(10));
+			ThirdBee.StartAnimation(imageNames, TimeSpan.FromMilliseconds(100));
+		}
+	}
+}
